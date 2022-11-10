8027015e670eea8fbdbe14fc2400fc39eb379992
Gelan007 <anatoliy.fil033@gmail.com>
2022-11-10 19:51:14 +0200

    Добавлены стили для navbar

--- company-ecosystem-front/src/compontents/Navbar.jsx
+++ company-ecosystem-front/src/compontents/Navbar.jsx
@@ -14,38 +14,30 @@ import {
 const Navbar = () => {
     return (
         <header className={s.header}>
-            <div className="header__container">
+            <div className={s.headerContainer}>
                 <Link to={HOME_ROUTE}>
-                    <img src={logo} alt="Logo"/>
-                    <span className="logo__text">Ecosystem</span>
+                    <div className={s.logoBlock}>
+                        <img src={logo} alt="Logo"/>
+                        <div className={s.logo__text}>Ecosystem</div>
+                    </div>
                 </Link>
                 <nav className={s.menu}>
                     <ul className={s.menu__ul}>
                         <li className={s.menu__li}>
                             <NavLink to={EMPLOYERS_ROUTE} className={s.menu__link}>Employees</NavLink>
                         </li>
-                    </ul>
-                    <ul className={s.menu__ul}>
                         <li className={s.menu__li}>
                             <NavLink to={LOCATIONS_ROUTE} className={s.menu__link}>Locations</NavLink>
                         </li>
-                    </ul>
-                    <ul className={s.menu__ul}>
                         <li className={s.menu__li}>
                             <NavLink to={POSTS_ROUTE} className={s.menu__link}>Posts</NavLink>
                         </li>
-                    </ul>
-                    <ul className={s.menu__ul}>
                         <li className={s.menu__li}>
                             <NavLink to={THINGS_ROUTE} className={s.menu__link}>Things</NavLink>
                         </li>
-                    </ul>
-                    <ul className={s.menu__ul}>
                         <li className={s.menu__li}>
                             <NavLink to={PHOTO_THINGS_ROUTE} className={s.menu__link}>PhotoThings</NavLink>
                         </li>
-                    </ul>
-                    <ul className={s.menu__ul}>
                         <li className={s.menu__li}>
                             <NavLink to={QUESTIONNAIRES_ROUTE} className={s.menu__link}>Questionnaires</NavLink>
                         </li>
