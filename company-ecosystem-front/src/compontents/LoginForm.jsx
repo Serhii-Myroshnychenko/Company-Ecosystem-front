7d61aa375a4813ef3d67f9cebbce11875cf80e5b
Serhii-Myroshnychenko <serzh.miroshnichenko.122@gmail.com>
2022-11-15 23:08:11 +0200

    added login implementation

--- company-ecosystem-front/src/compontents/LoginForm.jsx
+++ company-ecosystem-front/src/compontents/LoginForm.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import InputPurple from "./UI/input/InputPurple";
 import ButtonPurple from "./UI/button/ButtonPurple";
 import s from "./styles/LoginForm.module.css"
+
 const LoginForm = () => {
+
+    const [Password, setPassword] = useState("");
+    const [Email, setEmail] = useState("");
+    const [Token, setToken] = useState("");
+
+    async function login(e) {
+        e.preventDefault();
+        let result = await fetch("https://localhost:7032/Account/authenticate", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({email: Email, password: Password}),
+        });
+        if(result.status === 200){
+            var employee = await result.json();
+            setToken(employee.token)
+        }
+        else{
+            alert("Неверные данные")
+        }
+    }
+
     return (
         <div className={s.signInContainer}>
             <h3 className={s.titleText}>Sign in</h3>
-            <form action="">
-                <InputPurple type="text" placeholder="Enter email or user name" />
-                <InputPurple style={{marginTop: "32px", marginBottom: "60px"}} type="password" placeholder="Password"/>
-                <ButtonPurple>Login</ButtonPurple>
-            </form>
+                <InputPurple type="text" placeholder="Enter email or user name" onChange={(e)=>setEmail(e.target.value)} />
+                <InputPurple style={{marginTop: "32px", marginBottom: "60px"}} type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
+                <ButtonPurple onClick={login}>Login</ButtonPurple>
         </div>
     );
 };
